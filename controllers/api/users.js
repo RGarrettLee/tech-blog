727ac06863b52930d5969468e97e6a275f6d48c7
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-07-27 23:55:49 -0400

    heroku update

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -30,4 +30,8 @@ router.post('/login', (req, res) => {
       });
 });
 
+router.post('/logout', (req, res) => {
+    
+})
+
 module.exports = router;
--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -4,10 +4,9 @@ const withAuth = require('../utils/auth');
 
 dashboard.get('/', withAuth, async (req, res) => { // show all my posts | kick user to login screen if not logged in
     try {
-        const userData = await User.findByPk(req.session.user_id, {include: [Post]});
-        const user = userData.map((user) => user.get({ plain : true }));
-        console.info(user);
-        res.render('dashboard', { user, logged_in: req.session.logged_in });
+        const postData = await Post.findAll({ include: [User]});
+        const posts = postData.map((post) => post.get({ plain: true }));
+        res.render('dashboard', { posts, logged_in: req.session.logged_in });
     } catch (err) {
         res.json(err);
     }
