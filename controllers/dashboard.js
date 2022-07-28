be3bfc27ae0fed67b7d3c41d47d52e187f060ad9
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-07-28 11:11:48 -0400

    see users posts

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -5,14 +5,14 @@ const withAuth = require('../utils/auth');
 dashboard.get('/', withAuth, async (req, res) => { // show all my posts | kick user to login screen if not logged in
     try {
         if (req.session.logged_in) {
-            const userData = await User.findByPk(req.session.user_id);
+            const userData = await User.findByPk(req.session.user_id, { include: [Post] });
             const user = userData.get({ plain: true });
             console.info(user);
-            const postData = await Post.findAll({ include: [User]});
-            const posts = postData.map((post) => post.get({ plain: true }));
+            const posts = user.posts;
+            //console.info(posts);
             res.render('dashboard', { posts, logged_in: req.session.logged_in });
         } else {
-            window.location.replace('/login');
+            res.redirect('/login');
         }
     } catch (err) {
         res.json(err);
